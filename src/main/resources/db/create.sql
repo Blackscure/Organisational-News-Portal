6b6173e837c6fc18a9912505e039a7d3faf23c90
Blackscure <Wekesabuyahi@gmail.com>
2020-05-11 13:22:33 +0300

    create sql file

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE organisational_api;
+\c organisational_api;
+
+CREATE TABLE IF NOT EXISTS departments (id serial PRIMARY KEY, name varchar, description varchar, numberofemployees INTEGER);
+
+
+CREATE TABLE IF NOT EXISTS news (
+ id serial PRIMARY KEY ,
+ content VARCHAR,
+ description VARCHAR,
+ departmentid int
+);
+
+CREATE TABLE IF NOT EXISTS userName (
+ id serial PRIMARY KEY ,
+ name VARCHAR,
+ departmentid int,
+ role VARCHAR
+);
+
+CREATE DATABASE organisational_api_test WITH TEMPLATE organisational_api;
+
