9a2c11a8b81502761d693f888a41eed4c0cd0620
Blackscure <Wekesabuyahi@gmail.com>
2020-05-11 12:43:25 +0300

    user instatotes correctly

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,4 +1,13 @@
 package models;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserTest {
+    User user = new User("Moses", 1, "Maintain servers");
+    @Test
+    public void userInatanciatesCorrectly() {
+
+        assertEquals(true, user instanceof User);
+    }
 }
