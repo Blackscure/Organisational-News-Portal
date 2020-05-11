ad1ce5139b0a782a32a08564616e8107f17631c1
Blackscure <Wekesabuyahi@gmail.com>
2020-05-11 13:12:57 +0300

    test file with setters and getters

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,23 +1,26 @@
 package models;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class NewsTest {
     News news = new News("Sammsung S11+", "Samsung leaks images of the upcoming s11+", 1);
+
     @Test
     public void getsInstanceOfNews() {
         assertEquals(true,news instanceof News);
     }
+
     @Test
     public void getsContent() {
         assertEquals("Sammsung S11+", news.getContent());
     }
+
     @Test
     public void getDescription() {
         assertEquals("Samsung leaks images of the upcoming s11+", news.getDescription());
     }
+
     @Test
     public void getsDepartmentId() {
         assertEquals(1, news.getDepartmentid());
