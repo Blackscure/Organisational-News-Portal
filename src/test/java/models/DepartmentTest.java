2dba3a21e68c37c088f02d555a2f215d7c74f823
Blackscure <Wekesabuyahi@gmail.com>
2020-05-11 13:01:08 +0300

    getter method for gets deparment name,dep decsription, dep emplyee

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,4 +1,30 @@
-package models;
+ackage models;
 
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class DepartmentTest {
+    @Test
+    public void departmentInstanciatesCorrectly() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals(true, department instanceof Department);
+    }
+
+    @Test
+    public void getsDepartmentName() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals("Accounting", department.getDepartmentName());
+    }
+
+    @Test
+    public void getsDepartmentDescription() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals("Deals with Accounting", department.getdepartmentDescription());
+    }
+
+    @Test
+    public void getsDepartmentEmployees() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        assertEquals(50, department.getNumberOfEmployees());
+    }
 }
