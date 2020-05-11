cd76dc372d184eef49086440f04c5152a65c27e1
Blackscure <Wekesabuyahi@gmail.com>
2020-05-11 13:02:46 +0300

    setter method for gets deparment name,dep decsription, dep emplyee

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -27,4 +27,29 @@ public class DepartmentTest {
         Department department = new Department("Accounting", "Deals with Accounting", 50);
         assertEquals(50, department.getNumberOfEmployees());
     }
+
+    @Test
+    public void setsDepartmentName() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setDepartmentName("Accounting");
+        assertEquals("Accounting", department.getDepartmentName());
+    }
+    @Test
+    public void setsDepartmentDescription() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setDepartmentDescription("Deals with Accounting");
+        assertEquals("Deals with Accounting", department.getdepartmentDescription());
+    }
+    @Test
+    public void setsDepartmentEmployees() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setNumberOfEmployees(50);
+        assertEquals(50, department.getNumberOfEmployees());
+    }
+    @Test
+    public void setId() {
+        Department department = new Department("Accounting", "Deals with Accounting", 50);
+        department.setId(5);
+        assertEquals(5, department.getId());
+    }
 }
