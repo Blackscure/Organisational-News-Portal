c6948bc634382af2908673bc633f5a6cbe692904
Blackscure <Wekesabuyahi@gmail.com>
2020-05-11 12:48:50 +0300

    setter method for name,userdepartmentid and user role added

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -23,4 +23,24 @@ public class UserTest {
     public void getsUserRole() {
         assertEquals("Maintain servers", user.getRole());
     }
+
+    public void setsUserName() {
+        user.setName("Moses");
+        assertEquals("Moses", user.getName());
+    }
+    @Test
+    public void setsUserDepartmentId() {
+        user.setDepartmentId(1);
+        assertEquals(1, user.getDepartmentId());
+    }
+    @Test
+    public void setsUserRole() {
+        user.setName("Maintain servers");
+        assertEquals("Maintain servers", user.getRole());
+    }
+    @Test
+    public void setId() {
+        user.setId(5);
+        assertEquals(5, user.getId());
+    }
 }
