25b32c3145d1241f8d8cce9782deb7195fe6c963
Blackscure <Wekesabuyahi@gmail.com>
2020-05-12 12:59:45 +0300

    folders

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -13,13 +13,13 @@ public class DB {
             if (System.getenv("DATABASE_URL") == null) {
                 dbUri = new URI("jdbc:postgresql://localhost:5432/organisational_api");
                 String connectionString = "jdbc:postgresql://localhost:5432/organisational_api";
-                sql2o=new Sql2o(connectionString, "buyahi", "1234");
+                sql2o=new Sql2o(connectionString, "postgres", "1234");
             } else {
                 dbUri = new URI(System.getenv("DATABASE_URL"));
                 int port = dbUri.getPort();
                 String host = dbUri.getHost();
                 String path = dbUri.getPath();
-                String username = (dbUri.getUserInfo() == null) ? "buyahi" : dbUri.getUserInfo().split(":")[0];
+                String username = (dbUri.getUserInfo() == null) ? "postgres" : dbUri.getUserInfo().split(":")[0];
                 String password = (dbUri.getUserInfo() == null) ? "1234" : dbUri.getUserInfo().split(":")[1];
                 sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
             }
